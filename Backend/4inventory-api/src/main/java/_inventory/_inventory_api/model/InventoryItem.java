b0bd51e688dcfa9f4a7832fb1a7d086046289f7a
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-07-30 18:06:37 -0300

    feat: Add auto generate for created_at and last_update
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/model/InventoryItem.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/model/InventoryItem.java
@@ -2,7 +2,6 @@ package _inventory._inventory_api.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Generated;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,7 +10,7 @@ import java.util.UUID;
 @Data
 @Entity
 @Table(name = "tb_inventory")
-public class Inventory {
+public class InventoryItem {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -20,4 +19,15 @@ public class Inventory {
     private Integer quantity;
     private LocalDate created_at;
     private LocalDateTime last_update;
+
+    @PrePersist
+    public void prePersist(){
+        this.created_at = LocalDate.now();
+        this.last_update = LocalDateTime.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        this.last_update = LocalDateTime.now();
+    }
+
 }
