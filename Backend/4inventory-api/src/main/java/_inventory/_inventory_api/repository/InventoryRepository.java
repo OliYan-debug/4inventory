71963f784cada17b000e19dd61248d1bf629ee23
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-07-30 18:07:58 -0300

    fix: entity rename
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repository/InventoryRepository.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repository/InventoryRepository.java
@@ -1,11 +1,11 @@
 package _inventory._inventory_api.repository;
 
-import _inventory._inventory_api.model.Inventory;
+import _inventory._inventory_api.model.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface InventoryRepository extends JpaRepository<Inventory, UUID> {
-    
+public interface InventoryRepository extends JpaRepository<InventoryItem, UUID> {
+
 }
