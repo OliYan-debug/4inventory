9e901eec7b5af1599abb2b08c2f96cfc0f861420
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-08-29 17:59:26 -0300

    feat: add category repository
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repository/CategoryRepository.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package _inventory._inventory_api.repository;
 
-public class CategoryRepository {
+import _inventory._inventory_api.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
