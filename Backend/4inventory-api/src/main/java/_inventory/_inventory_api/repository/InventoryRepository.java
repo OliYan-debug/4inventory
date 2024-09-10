a8163c1109a273fcbe4a6a00eea8e435ea464dbc
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-09-10 17:40:11 -0300

    fix: change the ID from UUID to Long
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repository/InventoryRepository.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repository/InventoryRepository.java
@@ -4,8 +4,6 @@ import _inventory._inventory_api.model.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 @Repository
-public interface InventoryRepository extends JpaRepository<InventoryItem, UUID> {
-
+public interface InventoryRepository extends JpaRepository<InventoryItem, Long> {
 }
