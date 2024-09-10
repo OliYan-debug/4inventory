607c39c03d8082861e93c5f0cb6ba807faad4a7a
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-09-10 17:57:24 -0300

    feat: add category update endpoint
    solve issue #2
    
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controller/CategoryController.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controller/CategoryController.java
@@ -4,9 +4,12 @@ import _inventory._inventory_api.model.Category;
 import _inventory._inventory_api.repository.CategoryRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -22,6 +25,16 @@ public class CategoryController {
     @PostMapping("/add")
     public Category add(@RequestBody Category category){
         return categoryRepository.save(category);
+    }
+
+    @Operation(summary = "Update a category")
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Category categoryUpdate){
+        Optional<Category> optionalItem = categoryRepository.findById(categoryUpdate.getId());
+        if(optionalItem.isPresent()){
+            return ResponseEntity.ok(categoryRepository.save(categoryUpdate));
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The category id "+categoryUpdate.getId()+" not found");
     }
 }
 
