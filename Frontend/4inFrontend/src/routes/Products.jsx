e68fc11fa2662d093a1ab0e71fb70182213a6dd3
aandreluis <andreluisstn@gmail.com>
2024-09-15 17:53:00 -0300

    feat: Add the categories on the new product form

--- Frontend/4inFrontend/src/routes/Products.jsx
+++ Frontend/4inFrontend/src/routes/Products.jsx
@@ -1,91 +1,112 @@
-import { ArrowDownUp } from "lucide-react"
-import Item from "../components/Item"
-import Header from "../components/Header"
+import { ArrowDownUp } from "lucide-react";
+import Item from "../components/Item";
+import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
 
 export default function Products() {
-  const items = [
-    {
-      id: 1,
-      name: "Iphone",
-      description: "asishisissasasasasasss",
-      category: "Teste",
-      quantity: 102,
-      dateEntry: "15-08-2024",
-    },
-    {
-      id: 2,
-      name: "Iphone",
-      description: "asishisissasasasasasss",
-      category: "Teste",
-      quantity: 102,
-      dateEntry: "15-08-2024",
-    },
-    {
-      id: 3,
-      name: "Iphone",
-      description: "asishisissasasasasasss",
-      category: "Teste",
-      quantity: 102,
-      dateEntry: "15-08-2024",
-    },
-  ]
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/inventory")
+      .then((response) => {
+        setItems(response.items);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar dados:", error);
+      });
+  }, []);
+
+  // const items = [
+  //   {
+  //     id: 1,
+  //     name: "Iphone",
+  //     description: "asishisissasasasasasss",
+  //     category: "Teste",
+  //     quantity: 102,
+  //     dateEntry: "15-08-2024",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Iphone",
+  //     description: "asishisissasasasasasss",
+  //     category: "Teste",
+  //     quantity: 102,
+  //     dateEntry: "15-08-2024",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Iphone",
+  //     description: "asishisissasasasasasss",
+  //     category: "Teste",
+  //     quantity: 102,
+  //     dateEntry: "15-08-2024",
+  //   },
+  // ]
 
   const subtitle = () => {
     return (
-      <p className="text-sm text-neutral-500 mt-1">
+      <p className="mt-1 text-sm text-neutral-500">
         Found: <span className="font-bold">{items.length}</span>
       </p>
-    )
-  }
+    );
+  };
   return (
     <div className="flex flex-col gap-4">
       <Header title={"Products"} subtitle={subtitle()} />
 
-      <div className="max-w-full min-h-screen p-4 bg-neutral-50 rounded-2xl">
-        <div className="grid grid-cols-6 grid-rows-1 justify-items-center mb-2">
+      <div className="min-h-screen max-w-full rounded-2xl bg-neutral-50 p-4">
+        <div className="mb-2 grid grid-cols-6 grid-rows-1 justify-items-center">
           <div className="col-auto flex items-center">
-            <p className="text-neutral-600 font-bold">ID</p>
+            <p className="font-bold text-neutral-600">ID</p>
             <ArrowDownUp size={16} color="#525252" className="ms-1" />
           </div>
 
           <div className="col-auto flex items-center">
-            <p className="text-neutral-600 font-bold">Name</p>
+            <p className="font-bold text-neutral-600">Name</p>
             <ArrowDownUp size={16} color="#525252" className="ms-1" />
           </div>
 
           <div className="col-auto flex items-center">
-            <p className="text-neutral-600 font-bold">Description</p>
+            <p className="font-bold text-neutral-600">Description</p>
             <ArrowDownUp size={16} color="#525252" className="ms-1" />
           </div>
 
           <div className="col-auto flex items-center">
-            <p className="text-neutral-600 font-bold">Category</p>
+            <p className="font-bold text-neutral-600">Category</p>
             <ArrowDownUp size={16} color="#525252" className="ms-1" />
           </div>
 
           <div className="col-auto flex items-center">
-            <p className="text-neutral-600 font-bold">Quantity</p>
+            <p className="font-bold text-neutral-600">Quantity</p>
             <ArrowDownUp size={16} color="#525252" className="ms-1" />
           </div>
 
           <div className="col-auto flex items-center">
-            <p className="text-neutral-600 font-bold">Date of entry</p>
+            <p className="font-bold text-neutral-600">Date of entry</p>
             <ArrowDownUp size={16} color="#525252" className="ms-1" />
           </div>
         </div>
 
-        {items.map((item) => (
-          <Item
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            description={item.description}
-            category={item.category}
-            quantity={item.quantity}
-            dateEntry={item.dateEntry}
-          />
-        ))}
+        {items.length <= 0 ? (
+          <>
+            <p className="text-center">No items found</p>
+          </>
+        ) : (
+          items.map((item) => (
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              category={item.category}
+              quantity={item.quantity}
+              dateEntry={item.dateEntry}
+            />
+          ))
+        )}
       </div>
     </div>
-  )
+  );
 }
