821f947dcf291ca00588d26816f284b0e9281554
aandreluis <andreluisstn@gmail.com>
2024-09-21 21:51:23 -0300

    feat: add react-toastify

--- Frontend/4inFrontend/src/routes/Root.jsx
+++ Frontend/4inFrontend/src/routes/Root.jsx
@@ -1,16 +1,19 @@
-import { Outlet } from "react-router-dom"
-import NavBar from "../components/NavBar"
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function root() {
   return (
-    <div className="bg-neutral-300 max-w-full p-4 flex flex-row gap-4">
+    <div className="flex max-w-full flex-row gap-4 bg-neutral-300 p-4">
       <NavBar />
 
-      <div id="content" className="w-full h-full">
+      <div id="content" className="h-full w-full">
         <Outlet />
       </div>
+      <ToastContainer />
     </div>
-  )
+  );
 }
 
-export default root
+export default root;
