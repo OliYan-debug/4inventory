fee4ddb92af2c8ed0f58bcfa743f27702bda81f3
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-09-26 17:19:14 -0300

    test: Add some enpoints tests

---
+++ Backend/4inventory-api/src/test/java/_inventory/_inventory_api/controller/SearchControllerTest.java
@@ -0,0 +1,17 @@
+package _inventory._inventory_api.controller;
+
+import _inventory._inventory_api.repository.InventoryRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SearchControllerTest {
+
+    @Mock
+    InventoryRepository inventoryRepository;
+
+
+}
