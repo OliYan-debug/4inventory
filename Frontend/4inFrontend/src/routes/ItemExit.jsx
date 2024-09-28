39a2fe94b8071140ff9d3161489c2199ccaef8d4
aandreluis <andreluisstn@gmail.com>
2024-09-27 22:37:01 -0300

    feat: implements functionalities to update the items quantity

--- Frontend/4inFrontend/src/routes/ItemExit.jsx
+++ Frontend/4inFrontend/src/routes/ItemExit.jsx
@@ -1,69 +1,117 @@
-import { Link } from "react-router-dom"
-import Header from "../components/Header"
-import { ChevronRight, CircleMinus, ListRestart } from "lucide-react"
-import { useForm } from "react-hook-form"
+import { Link, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import { ChevronRight, CircleMinus, Undo2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { toast } from "react-toastify";
 
 export default function ItemExit() {
+  let { itemId } = useParams();
+  const [item, setItem] = useState([]);
+  const [updated, setUpdated] = useState(false);
+
+  useEffect(() => {
+    api
+      .get("/inventory/")
+      .then((response) => {
+        let itemFind = response.data.find((item) => item.id === Number(itemId));
+        setItem(itemFind);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar dados:", error);
+      });
+  }, [itemId, item, updated]);
+
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm({
     mode: "onChange",
-  })
+  });
 
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = async ({ quantity }) => {
+    if (Number.isNaN(quantity)) {
+      toast.error("Quantity is invalid");
+      return;
+    }
+
+    if (quantity > item.quantity) {
+      toast.warning("Quantity requested is higher than current");
+      return;
+    }
+
+    quantity = item.quantity - quantity;
+
+    try {
+      const response = await toast.promise(
+        api.put("/inventory/update/quantity", {
+          id: itemId,
+          quantity,
+        }),
+        {
+          pending: "Updating quantity...",
+          success: {
+            render() {
+              return <p>Quantity removed!</p>;
+            },
+          },
+          error: {
+            render({ data }) {
+              return (
+                <p>
+                  Error when remove:
+                  <span className="font-bold">{data.response.data}</span>. Try
+                  again.
+                </p>
+              );
+            },
+          },
+        },
+      );
+      setValue("quantity", 0);
+      setUpdated(true);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const subtitle = () => {
     return (
-      <p className="flex items-center text-sm text-neutral-500 mt-1">
+      <p className="mt-1 flex items-center text-sm text-neutral-500">
         <Link to={`/products`} className="hover:font-semibold">
           Producs
         </Link>
         <ChevronRight size={16} color="#737373" />
         <span className="font-semibold">Item Exit</span>
       </p>
-    )
-  }
+    );
+  };
 
   return (
     <div className="flex flex-col gap-4">
       <Header title={"Item Exit"} subtitle={subtitle()} />
 
-      <div className="max-w-full min-h-screen p-4 bg-neutral-50 rounded-2xl">
+      <div className="min-h-screen max-w-full rounded-2xl bg-neutral-50 p-4">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col pt-8 px-32 items-center gap-2"
+          className="flex flex-col items-center gap-2 px-32 pt-8"
         >
           <div className="w-full">
             <label htmlFor="item" className="text-sm text-neutral-500">
-              Item Name or ID*
+              Item Name*
             </label>
             <input
-              defaultValue=""
-              {...register("item", {
-                required: "Item name is required",
-                maxLength: {
-                  value: 20,
-                  message: "Maximum character value exceeded",
-                },
-              })}
-              aria-invalid={errors.item ? "true" : "false"}
+              defaultValue={item.item}
+              disabled={true}
+              {...register("item")}
               type="text"
               id="item"
-              className={`w-full rounded-lg border border-neutral-400 px-4 py-2
-            text-neutral-500 outline-none hover:border-neutral-500 focus-visible::border-neutral-500
-              disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400
-              ${
-                errors.item &&
-                "border-red-600 bg-red-100 text-red-600 hover:border-red-600 focus-visible::border-red-600"
-              }`}
+              className={`focus-visible::border-neutral-500 w-full rounded-lg border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400`}
             />
-            {errors.item && (
-              <p role="alert" className="mt-1 text-center text-xs text-red-600">
-                {errors.item?.message}
-              </p>
-            )}
           </div>
 
           <div className="w-full">
@@ -73,7 +121,7 @@ export default function ItemExit() {
             <textarea
               defaultValue=""
               {...register("justification", {
-                required: "Justification is required",
+                // required: "Justification is required",
                 maxLength: {
                   value: 255,
                   message: "Maximum character value exceeded",
@@ -82,12 +130,10 @@ export default function ItemExit() {
               aria-invalid={errors.justification ? "true" : "false"}
               type="text"
               id="justification"
-              className={`w-full h-26 resize-none rounded-lg border border-neutral-400 px-4 py-2
-            text-neutral-500 outline-none hover:border-neutral-500 focus-visible::border-neutral-500
-              disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400
-              ${
+              disabled={true}
+              className={`h-26 focus-visible::border-neutral-500 w-full resize-none rounded-lg border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400 ${
                 errors.justification &&
-                "border-red-600 bg-red-100 text-red-600 hover:border-red-600 focus-visible::border-red-600"
+                "focus-visible::border-red-600 border-red-600 bg-red-100 text-red-600 hover:border-red-600"
               }`}
             ></textarea>
             {errors.justification && (
@@ -99,25 +145,32 @@ export default function ItemExit() {
 
           <div className="w-full">
             <label htmlFor="quantity" className="text-sm text-neutral-500">
-              Quantity*
+              Remove* (Current:
+              <span className="font-medium"> {item.quantity}</span>)
             </label>
             <input
               defaultValue="0"
               {...register("quantity", {
-                maxLength: {
-                  value: 20,
+                required: "Quantity is required",
+                max: {
+                  value: 9999999999,
                   message: "Maximum character value exceeded",
                 },
+                min: {
+                  value: 1,
+                  message: "Min. quantity to add is 1",
+                },
+                valueAsNumber: true,
+                validate: (value) =>
+                  value <= item.quantity ||
+                  "Quantity requested is higher than current",
               })}
               aria-invalid={errors.quantity ? "true" : "false"}
               type="number"
               id="quantity"
-              className={`w-full rounded-lg border border-neutral-400 px-4 py-2
-            text-neutral-500 outline-none hover:border-neutral-500 focus-visible::border-neutral-500
-              disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400
-              ${
+              className={`focus-visible::border-neutral-500 w-full rounded-lg border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400 ${
                 errors.quantity &&
-                "border-red-600 bg-red-100 text-red-600 hover:border-red-600 focus-visible::border-red-600"
+                "focus-visible::border-red-600 border-red-600 bg-red-100 text-red-600 hover:border-red-600"
               }`}
             />
             {errors.quantity && (
@@ -129,20 +182,20 @@ export default function ItemExit() {
 
           <button
             type="submit"
-            className="w-2/5 mt-10 py-2 rounded-lg bg-red-400 text-neutral-50 font-semibold flex items-center justify-center px-4 hover:bg-red-500 transition"
+            className="mt-10 flex w-2/5 items-center justify-center rounded-lg bg-red-400 px-4 py-2 font-semibold text-neutral-50 transition hover:bg-red-500"
           >
             <CircleMinus size={20} color="#fafafa" className="me-2" />
             Item exit
           </button>
-          <button
-            type="reset"
-            className="font-semibold text-neutral-400 hover:underline hover:opacity-80 flex items-center"
+          <Link
+            to={"/products"}
+            className="flex items-center font-semibold text-neutral-400 hover:underline hover:opacity-80"
           >
-            Cancel
-            <ListRestart size={20} color="#a3a3a3" className="ms-1" />
-          </button>
+            Back to products
+            <Undo2 size={20} color="#a3a3a3" className="ms-1" />
+          </Link>
         </form>
       </div>
     </div>
-  )
+  );
 }
