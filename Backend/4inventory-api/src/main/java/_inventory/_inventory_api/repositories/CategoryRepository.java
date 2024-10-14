ca61414548c26c2c942b08ccf577c96c5519beb1
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-10-14 16:38:05 -0300

    refactor(CategoryRepository): change return type of findByNameIgnoreCase()
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repositories/CategoryRepository.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repositories/CategoryRepository.java
@@ -4,7 +4,9 @@ import _inventory._inventory_api.models.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByNameIgnoreCase(String name);
+    List<Category> findByNameIgnoreCase(String name);
 }
