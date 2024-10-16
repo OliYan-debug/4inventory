7e2a2032f9ec46638a56b6c7f6f1a82fc10fbebd
aandreluis <andreluisstn@gmail.com>
2024-10-15 22:31:26 -0300

    feat: adds item search and justification, it is now possible to entry using search or URL

--- Frontend/4inFrontend/src/routes/ItemEntry.jsx
+++ Frontend/4inFrontend/src/routes/ItemEntry.jsx
@@ -1,80 +1,154 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
-import { ChevronRight, CirclePlus, Undo2 } from "lucide-react";
+import { ChevronRight, CirclePlus, SearchIcon, Undo2 } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { api } from "../services/api";
 import { toast } from "react-toastify";
+import ItemSearch from "../components/ItemSearch";
 
 export default function ItemEntry() {
   let { itemId } = useParams();
-  const [item, setItem] = useState([]);
-  const [updated, setUpdated] = useState(false);
-
-  useEffect(() => {
-    api
-      .get("/inventory/")
-      .then((response) => {
-        let itemFind = response.data.find((item) => item.id === Number(itemId));
-        setItem(itemFind);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar dados:", error);
-      });
-  }, [itemId, item, updated]);
+  const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState([]);
+  const ref = useRef(null);
+  const navigate = useNavigate();
 
   const {
     register,
     handleSubmit,
     setValue,
+    clearErrors,
     formState: { errors },
   } = useForm({
     mode: "onChange",
   });
 
-  const onSubmit = async ({ quantity }) => {
-    console.log({ itemId, quantity });
-    if (Number.isNaN(quantity)) {
+  useEffect(() => {
+    if (itemId) {
+      api
+        .get(`/inventory/item/${itemId}`)
+        .then((response) => {
+          setSelectedItem(response.data);
+          setValue("item", selectedItem.item);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          toast.warning(
+            <p>
+              <span className="font-bold">{error.response.data.message}</span>{" "}
+              search other item{" "}
+            </p>,
+            {
+              toastId: itemId,
+            },
+          );
+
+          navigate("/products/entry");
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [itemId, navigate, selectedItem.item]);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setItems("");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  const onSubmit = async (data) => {
+    console.log(selectedItem);
+    if (Number.isNaN(data.quantity)) {
       toast.error("Quantity is invalid");
       return;
     }
 
+    data.id = selectedItem.id;
+    delete data.item;
+
+    data.quantity += selectedItem.quantity;
+
     try {
-      const response = await toast.promise(
-        api.put("/inventory/update/quantity", { id: itemId, quantity }),
-        {
-          pending: "Updating quantity...",
-          success: {
-            render() {
-              return <p>Quantity added!</p>;
-            },
-          },
-          error: {
-            render({ data }) {
-              return (
-                <p>
-                  Error when adding:
-                  <span className="font-bold">{data.response.data.error}</span>.
-                  Try again.
-                </p>
-              );
-            },
+      await toast.promise(api.put("/inventory/update/quantity", data), {
+        pending: "Updating quantity...",
+        success: {
+          render() {
+            return <p>Quantity added!</p>;
           },
         },
-      );
+        error: {
+          render({ data }) {
+            return (
+              <p>
+                Error when adding:
+                <span className="font-bold">{data.response.data.message}</span>.
+                Try again.
+              </p>
+            );
+          },
+        },
+      });
+
+      selectedItem.quantity = data.quantity;
       setValue("quantity", 0);
-      setUpdated(true);
-      console.log(response);
+      setValue("justification", "");
+      setItems("");
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleSearchProduct = async (event) => {
+    errors && clearErrors();
+
+    if (event.target.value === "") {
+      return;
+    }
+
+    let search = event.target.value;
+
+    try {
+      const response = await api.get(`/search/${search}`);
+      setItems(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  function handleSelect(id) {
+    if (itemId) {
+      navigate(`/products/entry/${id}`);
+    }
+
+    const itemFind = items.find((item) => item.id === Number(id));
+    setValue("item", itemFind.item);
+    setItems("");
+    setSelectedItem(itemFind);
+  }
+
+  async function handleSearch() {
+    await api
+      .get("/inventory/")
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar dados:", error);
+      });
+  }
+
   const subtitle = () => {
     return (
       <p className="mt-1 flex items-center text-sm text-neutral-500">
         <Link to={`/products`} className="hover:font-semibold">
-          Producs
+          Products
         </Link>
         <ChevronRight size={16} color="#737373" />
         <span className="font-semibold">Item Entry</span>
@@ -91,24 +165,76 @@ export default function ItemEntry() {
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col items-center gap-2 px-32 pt-8"
         >
-          <div className="w-full">
+          <div className="relative w-full">
             <label htmlFor="item" className="text-sm text-neutral-500">
               Item Name*
             </label>
-            <input
-              defaultValue={item.item}
-              disabled={true}
-              {...register("item")}
-              type="text"
-              id="item"
-              className={`focus-visible::border-neutral-500 w-full rounded-lg border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400`}
-            />
+            <div className="relative flex items-center">
+              <input
+                defaultValue={itemId ? items.item : ""}
+                {...register("item", {
+                  required: "Item name is required",
+                  maxLength: {
+                    value: 20,
+                    message: "Maximum character value exceeded",
+                  },
+                })}
+                aria-invalid={errors.item ? "true" : "false"}
+                type="text"
+                id="item"
+                onChange={handleSearchProduct}
+                placeholder="Search items"
+                className={`focus-visible::border-neutral-500 ${items.length > 0 ? "rounded-t-lg border-b-0" : "rounded-lg"} w-full border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400 ${
+                  errors.item &&
+                  "focus-visible::border-red-600 border-red-600 bg-red-100 text-red-600 hover:border-red-600"
+                }`}
+              />
+
+              <button
+                type="button"
+                className="absolute right-2 hover:opacity-50"
+                onClick={() => {
+                  errors && clearErrors();
+                  handleSearch();
+                }}
+              >
+                <SearchIcon size={20} color="#737373" />
+              </button>
+            </div>
+
+            {errors.item && (
+              <p role="alert" className="mt-1 text-center text-xs text-red-600">
+                {errors.item?.message}
+              </p>
+            )}
+
+            {items.length > 0 && (
+              <div
+                ref={ref}
+                className="absolute max-h-36 w-full overflow-y-auto rounded-b-lg border border-neutral-400 bg-neutral-50 py-2 text-neutral-500 shadow-md transition"
+              >
+                {items.length > 0 && (
+                  <ul className="flex w-full flex-col justify-items-center gap-px">
+                    {items.map((item) => {
+                      return (
+                        <ItemSearch
+                          key={item.id}
+                          id={item.id}
+                          item={item.item}
+                          handleSelect={handleSelect}
+                        />
+                      );
+                    })}
+                  </ul>
+                )}
+              </div>
+            )}
           </div>
 
           <div className="w-full">
             <label htmlFor="quantity" className="text-sm text-neutral-500">
               Add* (Current:
-              <span className="font-medium"> {item.quantity}</span>)
+              <span className="font-medium"> {selectedItem.quantity}</span>)
             </label>
             <input
               defaultValue="0"
@@ -127,6 +253,7 @@ export default function ItemEntry() {
               aria-invalid={errors.quantity ? "true" : "false"}
               type="number"
               id="quantity"
+              disabled={selectedItem.length === 0}
               className={`focus-visible::border-neutral-500 w-full rounded-lg border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400 ${
                 errors.quantity &&
                 "focus-visible::border-red-600 border-red-600 bg-red-100 text-red-600 hover:border-red-600"
@@ -135,6 +262,35 @@ export default function ItemEntry() {
             {errors.quantity && (
               <p role="alert" className="mt-1 text-center text-xs text-red-600">
                 {errors.quantity?.message}
+              </p>
+            )}
+          </div>
+
+          <div className="w-full">
+            <label htmlFor="justification" className="text-sm text-neutral-500">
+              Justification*
+            </label>
+            <textarea
+              defaultValue=""
+              {...register("justification", {
+                required: "Justification is required",
+                maxLength: {
+                  value: 255,
+                  message: "Maximum character value exceeded",
+                },
+              })}
+              aria-invalid={errors.justification ? "true" : "false"}
+              type="text"
+              id="justification"
+              disabled={selectedItem.length === 0}
+              className={`h-26 focus-visible::border-neutral-500 w-full resize-none rounded-lg border border-neutral-400 px-4 py-2 text-neutral-500 outline-none hover:border-neutral-500 disabled:cursor-no-drop disabled:text-opacity-60 disabled:hover:border-neutral-400 ${
+                errors.justification &&
+                "focus-visible::border-red-600 border-red-600 bg-red-100 text-red-600 hover:border-red-600"
+              }`}
+            ></textarea>
+            {errors.justification && (
+              <p role="alert" className="mt-1 text-center text-xs text-red-600">
+                {errors.justification?.message}
               </p>
             )}
           </div>
