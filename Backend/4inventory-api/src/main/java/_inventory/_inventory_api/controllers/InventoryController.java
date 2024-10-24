848e69293afd7d9622640cd4d77da94b70cda188
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-10-24 18:51:53 -0300

    feat(controllers): Add paging and sort for the findAll method
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/InventoryController.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/InventoryController.java
@@ -13,6 +13,7 @@ import _inventory._inventory_api.models.records.MessageHandler;
 import _inventory._inventory_api.services.InventoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,8 +29,10 @@ public class InventoryController {
 
     @Operation(summary = "List all items in the inventory")
     @GetMapping("/")
-    public ResponseEntity<List<InventoryItem>> listInventory() {
-        return ResponseEntity.ok(inventoryService.findAll());
+    public ResponseEntity<Page<InventoryItem>> listInventory(@RequestParam(value = "sort", defaultValue = "id,desc") String sort,
+                                                             @RequestParam(value = "page", defaultValue = "0") int page,
+                                                             @RequestParam(value = "size", defaultValue = "10") int size) {
+        return ResponseEntity.ok(inventoryService.findAll(page, size, sort));
     }
 
     @Operation(summary = "Add a item in the inventory")
@@ -61,12 +64,13 @@ public class InventoryController {
             return ResponseEntity.badRequest().body(new MessageHandler(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
         }
     }
+
     @GetMapping(value = "/item/{id}")
     @Operation(summary = "Find a item by id")
     public ResponseEntity<?> findItemById(@PathVariable Long id) {
-        try{
+        try {
             return ResponseEntity.ok(inventoryService.findById(id));
-        }catch(ItemIdNotFoundException e){
+        } catch (ItemIdNotFoundException e) {
             return ResponseEntity.badRequest().body(new MessageHandler(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
         }
     }
--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/RegistryController.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/RegistryController.java
@@ -2,12 +2,15 @@ package _inventory._inventory_api.controllers;
 
 import _inventory._inventory_api.models.entities.Registry;
 import _inventory._inventory_api.repositories.RegistryRepository;
+import _inventory._inventory_api.services.RegistryService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,12 +20,14 @@ import java.util.List;
 public class RegistryController {
 
     @Autowired
-    RegistryRepository registryRepository;
+    RegistryService registryService;
 
     @Operation(summary = "Get all registries")
     @GetMapping("/")
-    public ResponseEntity<List<Registry>> findAll(){
-        return new ResponseEntity<>(registryRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<Page<Registry>> findAll(@RequestParam(value = "sort", defaultValue = "id,desc") String sort,
+                                                  @RequestParam(value = "page", defaultValue = "0") int page,
+                                                  @RequestParam(value = "size", defaultValue = "10") int size){
+        return new ResponseEntity<>(registryService.findAll(page, size, sort), HttpStatus.OK);
     }
 
 }
