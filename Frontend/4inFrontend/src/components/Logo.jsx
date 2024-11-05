ccb739f002018ebc0495f538fc2d7b72ce29dd52
aandreluis <andreluisstn@gmail.com>
2024-11-05 20:32:40 -0300

    feat: adds the mobile logo component
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

---
+++ Frontend/4inFrontend/src/components/Logo.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
+export default function Logo() {
+  return (
+    <div
+      className={
+        "mb-4 flex w-full rounded-xl bg-neutral-800 text-neutral-50 transition-all md:hidden"
+      }
+    >
+      <div className="flex w-full flex-col items-center py-2 transition-all">
+        <div className="flex flex-col items-center">
+          <Link to={"/"}>
+            <img src={logo} alt="4inventory" />
+          </Link>
+          <span className="mt-px text-xs font-thin text-neutral-200">
+            1.1.2
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
