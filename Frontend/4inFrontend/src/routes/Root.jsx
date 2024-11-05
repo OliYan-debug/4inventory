e31fe37558a647edac2490c86c2b7f7454711e5f
aandreluis <andreluisstn@gmail.com>
2024-11-05 20:31:19 -0300

    refac: small changes to implement responsive design
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/routes/Root.jsx
+++ Frontend/4inFrontend/src/routes/Root.jsx
@@ -2,13 +2,15 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Logo from "../components/Logo";
 
 function root() {
   return (
-    <div className="flex max-w-full flex-row gap-4 bg-neutral-300 p-4">
+    <div className="m-0 flex max-w-full flex-row bg-neutral-300 p-4">
       <NavBar />
 
       <div id="content" className="h-full w-full">
+        <Logo />
         <Outlet />
       </div>
       <ToastContainer />
