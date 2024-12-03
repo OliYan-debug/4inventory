e1c1695959a15c9edeebdcf2c5239b290f696bdd
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-12-03 12:17:31 -0300

    feat: Add an endpoint to reset user password
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/AuthenticationController.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/AuthenticationController.java
@@ -4,6 +4,7 @@ import _inventory._inventory_api.config.security.TokenService;
 import _inventory._inventory_api.domain.dto.AuthenticationDTO;
 import _inventory._inventory_api.domain.dto.LoginResponseDTO;
 import _inventory._inventory_api.domain.dto.RegisterDTO;
+import _inventory._inventory_api.domain.dto.ResetPasswordDTO;
 import _inventory._inventory_api.domain.entities.user.User;
 import _inventory._inventory_api.repositories.UserRepository;
 import jakarta.validation.Valid;
@@ -28,15 +29,29 @@ public class AuthenticationController {
     private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+    public ResponseEntity<Object> login(@RequestBody @Valid AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
         var token = tokenService.generateToken((User) auth.getPrincipal());
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
+    @PostMapping("/reset")
+    public ResponseEntity<Object> resetPassword(@RequestBody @Valid ResetPasswordDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        if(data.newPassword() == null) return ResponseEntity.badRequest().build();
+        var encryptedPassword = new BCryptPasswordEncoder().encode(data.newPassword());
+        var user = userRepository.findByUsername(data.login());
+        var id = user.getId();
+        var roles = user.getRole();
+        var userChanged = new User(id, data.login(), encryptedPassword, roles);
+        userRepository.save(userChanged);
+        return ResponseEntity.ok().build();
+    }
+
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+    public ResponseEntity<Object> register(@RequestBody @Valid RegisterDTO data){
         if(this.userRepository.findByUsername(data.login()) != null) return ResponseEntity.badRequest().build();
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
