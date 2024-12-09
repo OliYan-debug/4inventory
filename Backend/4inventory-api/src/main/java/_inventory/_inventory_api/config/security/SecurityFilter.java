1ba2e9e892a3534ad8ed32d93a96858014864302
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-12-09 09:43:34 -0300

    fix: add a new layer of verification because of null pointer exception
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/config/security/SecurityFilter.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/config/security/SecurityFilter.java
@@ -27,10 +27,11 @@ public class SecurityFilter extends OncePerRequestFilter {
         var token = this.recoverToken(request);
         if(token != null){
             var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByUsername(login);
-
-            var authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if(login != null) {
+                UserDetails user = userRepository.findByUsername(login);
+                var authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
         filterChain.doFilter(request, response);
     }
