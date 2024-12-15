d22242600a1d721a3e4a45dc6f241ab8b0de5b96
aandreluis <andreluisstn@gmail.com>
2024-12-14 23:05:24 -0300

    fix: the path in the pagination to reuse the component on the Dashborad page
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/Pagination.jsx
+++ Frontend/4inFrontend/src/components/Pagination.jsx
@@ -15,6 +15,7 @@ export default function Pagination({
   first,
   last,
   setSize,
+  path,
 }) {
   const navigate = useNavigate();
   const [cookies, setCookie] = useCookies();
@@ -45,7 +46,7 @@ export default function Pagination({
 
       pages.push(
         <Link
-          to={`/products/${i}`}
+          to={`/${path}/${i}`}
           key={i}
           aria-current="page"
           className={`${current ? "relative z-10 inline-flex items-center bg-neutral-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-600" : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-neutral-900 ring-1 ring-inset ring-neutral-300 hover:bg-neutral-50 focus:z-20 focus:outline-offset-0"}`}
@@ -62,7 +63,7 @@ export default function Pagination({
       maxAge: 60 * 60 * 24, //1 day
     });
     setSize(e.target.value);
-    navigate("/products");
+    navigate(`/${path}`);
   };
 
   const FormSelectSize = () => {
@@ -94,7 +95,7 @@ export default function Pagination({
     <div className="flex items-center justify-between border-t border-neutral-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
         <Link
-          to={`/products/${pageNumber - 1}`}
+          to={`/${path}/${pageNumber - 1}`}
           className={`relative inline-flex items-center rounded-lg border border-neutral-300 bg-white px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-neutral-50 ${first && "pointer-events-none opacity-70"}`}
         >
           Previous
@@ -103,7 +104,7 @@ export default function Pagination({
         <FormSelectSize />
 
         <Link
-          to={`/products/${pageNumber + 1}`}
+          to={`/${path}/${pageNumber + 1}`}
           className={`relative ml-3 inline-flex items-center rounded-lg border border-neutral-300 bg-white px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-neutral-50 ${last && "pointer-events-none opacity-70"}`}
         >
           Next
@@ -127,14 +128,14 @@ export default function Pagination({
               className="isolate inline-flex -space-x-px rounded-lg shadow-sm"
             >
               <Link
-                to={`/products`}
+                to={`/${path}`}
                 className={`relative inline-flex items-center rounded-l-lg px-2 py-2 text-neutral-400 ring-1 ring-inset ring-neutral-300 hover:bg-neutral-50 focus:z-20 focus:outline-offset-0 ${first && "pointer-events-none opacity-70"}`}
               >
                 <span className="sr-only">First</span>
                 <ChevronsLeft aria-hidden="true" className="h-5 w-5" />
               </Link>
               <Link
-                to={`/products/${pageNumber - 1}`}
+                to={`/${path}/${pageNumber - 1}`}
                 className={`relative inline-flex items-center px-2 py-2 text-neutral-400 ring-1 ring-inset ring-neutral-300 hover:bg-neutral-50 focus:z-20 focus:outline-offset-0 ${first && "pointer-events-none opacity-70"}`}
               >
                 <span className="sr-only">Previous</span>
@@ -154,14 +155,14 @@ export default function Pagination({
               )}
 
               <Link
-                to={`/products/${pageNumber + 1}`}
+                to={`/${path}/${pageNumber + 1}`}
                 className={`relative inline-flex items-center px-2 py-2 text-neutral-400 ring-1 ring-inset ring-neutral-300 hover:bg-neutral-50 focus:z-20 focus:outline-offset-0 ${last && "pointer-events-none opacity-70"}`}
               >
                 <span className="sr-only">Next</span>
                 <ChevronRight aria-hidden="true" className="h-5 w-5" />
               </Link>
               <Link
-                to={`/products/${totalPages - 1}`}
+                to={`/${path}/${totalPages - 1}`}
                 className={`relative inline-flex items-center rounded-r-lg px-2 py-2 text-neutral-400 ring-1 ring-inset ring-neutral-300 hover:bg-neutral-50 focus:z-20 focus:outline-offset-0 ${last && "pointer-events-none opacity-70"}`}
               >
                 <span className="sr-only">Last</span>
