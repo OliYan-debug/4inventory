0f06fabe7bd1344fdc7127c5d695c7e3b5af477a
aandreluis <andreluisstn@gmail.com>
2024-12-18 22:57:58 -0300

    fix: the version number
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/Logo.jsx
+++ Frontend/4inFrontend/src/components/Logo.jsx
@@ -14,7 +14,7 @@ export function Logo() {
             <img src={logo} alt="4inventory" />
           </Link>
           <span className="mt-px text-xs font-thin text-neutral-200">
-            1.3.5
+            1.3.6
           </span>
         </div>
       </div>
--- Frontend/4inFrontend/src/components/NavBar.jsx
+++ Frontend/4inFrontend/src/components/NavBar.jsx
@@ -53,7 +53,7 @@ export function NavBar() {
             />
           </Link>
           <span className="mt-px text-xs font-thin text-neutral-200">
-            1.3.5
+            1.3.6
           </span>
         </div>
 
