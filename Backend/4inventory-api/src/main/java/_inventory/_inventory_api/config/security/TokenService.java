983431efa4a10b9362aae1293472f832a38138d3
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-12-29 13:29:25 -0300

    refactor: change print to log
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/config/security/TokenService.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/config/security/TokenService.java
@@ -8,17 +8,16 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Map;
 
+@Slf4j
 @Service
 public class TokenService {
     @Value("${api.security.token.secret}")
@@ -73,7 +72,7 @@ public class TokenService {
                     .verify(token).getSubject();
             repository.save(new RevokedUserToken(tokenToRevoke, username));
         } catch (JWTVerificationException e) {
-            System.out.println(e);;
+            log.error("AUTH: ", e);;
         }
     }
 }
