eeaacf8c21a62e2e9cf07df093f24d6788de60b9
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2024-12-29 13:31:20 -0300

    refactor: Split the business logic to services
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/AuthenticationController.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/controllers/AuthenticationController.java
@@ -1,87 +1,60 @@
 package _inventory._inventory_api.controllers;
 
-import _inventory._inventory_api.config.security.TokenService;
 import _inventory._inventory_api.domain.dto.*;
-import _inventory._inventory_api.domain.entities.user.User;
-import _inventory._inventory_api.domain.enums.UserRoles;
-import _inventory._inventory_api.repositories.UserRepository;
+import _inventory._inventory_api.domain.utils.ResponseErrorHandler;
+import _inventory._inventory_api.services.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
-
-import java.util.Arrays;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private AuthenticationService authenticationService;
+
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private TokenService tokenService;
+    private ResponseErrorHandler responseErrorHandler;
 
     @Operation(summary = "User login")
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody @Valid AuthenticationDTO data){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.generateToken((User) auth.getPrincipal());
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+    public ResponseEntity<Object> login(@RequestBody @Valid AuthenticationDTO data) {
+        try {
+            return ResponseEntity.ok(new LoginResponseDTO(authenticationService.login(data)));
+        } catch (AuthenticationException e) {
+            return responseErrorHandler.generate(HttpStatus.BAD_REQUEST, "User or password invalid");
+        }
     }
 
     @Operation(summary = "User password reset")
     @PostMapping("/reset")
-    public ResponseEntity<Object> resetPassword(@RequestHeader(value = "authorization") String authHeader, @RequestBody @Valid ResetPasswordDTO data){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        if(data.newPassword() == null) return ResponseEntity.badRequest().build();
-        var encryptedPassword = new BCryptPasswordEncoder().encode(data.newPassword());
-        var user = userRepository.findByUsername(data.login());
-        user.setPassword(encryptedPassword);
-        userRepository.save(user);
-        var token = authHeader.split(" ")[1];
-        tokenService.revokeToken(token);
+    public ResponseEntity<Object> resetPassword(@RequestHeader(value = "authorization") String authHeader, @RequestBody @Valid ResetPasswordDTO data) {
+        authenticationService.resetPassword(authHeader, data);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "User register")
     @PostMapping("/register")
-    public ResponseEntity<Object> register(@RequestBody @Valid RegisterDTO data){
-        if(this.userRepository.findByUsername(data.login()) != null) return ResponseEntity.badRequest().build();
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        var newUser = new User(data.name(), data.login(), encryptedPassword, data.role());
-
-        this.userRepository.save(newUser);
-
+    public ResponseEntity<Object> register(@RequestBody @Valid RegisterDTO data) {
+        authenticationService.register(data);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Logout user ('Set a token as invalid')")
     @PostMapping("/logout")
-    public ResponseEntity<Object> logout(@RequestHeader(value = "authorization") String authHeader){
-        var token = authHeader.split(" ")[1];
-        tokenService.revokeToken(token);
+    public ResponseEntity<Object> logout(@RequestHeader(value = "authorization") String authHeader) {
+        authenticationService.logout(authHeader);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Change the user role")
     @PostMapping("/change-role")
-    public ResponseEntity<Object> changeRole(@RequestBody ChangeRoleDTO data){
-        var userDB = this.userRepository.findByUsername(data.login());
-        if(userDB == null || userDB.getUsername().equalsIgnoreCase("admin")) return ResponseEntity.badRequest().build();
-        userDB.setRole(data.role());
-        userRepository.save(userDB);
+    public ResponseEntity<Object> changeRole(@RequestBody ChangeRoleDTO data) {
+        authenticationService.changeUserRole(data);
         return ResponseEntity.ok().build();
     }
 }
