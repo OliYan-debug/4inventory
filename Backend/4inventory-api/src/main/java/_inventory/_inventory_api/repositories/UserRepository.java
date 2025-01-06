13b0b67365c783bcbecf7bee8852e1e4cef40018
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2025-01-06 11:26:47 -0300

    feat: Change repository to handle pages in db requests
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repositories/UserRepository.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package _inventory._inventory_api.repositories;
 
 import _inventory._inventory_api.domain.entities.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
     User findByUsername(String username);
+
+    void save(User user);
 }
