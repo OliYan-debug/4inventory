fdb239c4ecb16c2b03b31db249ab2c9b721d54a3
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2025-01-14 19:40:16 -0300

    feat: method to delete a user and some code refactoring
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/services/AdminService.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/services/AdminService.java
@@ -4,6 +4,7 @@ import _inventory._inventory_api.domain.dto.ChangeRoleDTO;
 import _inventory._inventory_api.domain.dto.ProfileDTO;
 import _inventory._inventory_api.domain.dto.ResetPasswordAdminDTO;
 import _inventory._inventory_api.domain.dto.UserDTO;
+import _inventory._inventory_api.domain.entities.user.User;
 import _inventory._inventory_api.domain.exceptions.security.InvalidAuthException;
 import _inventory._inventory_api.domain.exceptions.users.UserException;
 import _inventory._inventory_api.domain.utils.UserToken;
@@ -32,24 +33,38 @@ public class AdminService {
     public Page<UserDTO> getUsers(int page, int size, String sort) {
         Sort sortable = Sort.by(Sort.Direction.fromString(sort.split(",")[1]), sort.split(",")[0]);
         var pageable = PageRequest.of(page, size, sortable);
-        var users =  repository.findAll(pageable);
+        var users = repository.findAll(pageable);
         return users.map(user -> new UserDTO(user.getId(), user.getName(), user.getUsername(), user.getRole().getRole()));
     }
 
     public void resetPassword(ResetPasswordAdminDTO data) {
-        var user = repository.findByUsername(data.username());
-        if (data.username().equals("admin")) throw new InvalidAuthException("You can't change the admin default password");
+        var user = checkUsername(data.username());
         var encryptedPassword = new BCryptPasswordEncoder().encode(data.username().toUpperCase());
         user.setPassword(encryptedPassword);
         repository.save(user);
     }
 
+    public void deleteUser(ResetPasswordAdminDTO data){
+        var user = checkUsername(data.username());
+        repository.deleteById(user.getId());
+    }
+
     public void changeUserRole(ChangeRoleDTO data){
-        var userDB = this.repository.findByUsername(data.login());
-        if (userDB == null)
-            throw new UserException("Invalid Credentials, please try another user");
+        var userDB = checkUsername(data.login());
+        if(userDB.getRole().equals(data.role())) throw new UserException("User already have this role");
         userDB.setRole(data.role());
         repository.save(userDB);
+    }
+
+    private User checkUsername(String username){
+        var user = repository.findByUsername(username);
+        if (user == null) throw new UserException("Invalid Credentials, please try another user");
+        if (user.getUsername().equals("admin")) throw new InvalidAuthException("You can't change the default admin account");
+        if(user.getName() == null) {
+            user.setName("4inventory");
+            repository.save(user);
+        };
+        return user;
     }
 
 }
