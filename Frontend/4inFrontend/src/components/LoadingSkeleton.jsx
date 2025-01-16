5910254ec8f82e1efc544ec47f3faace0e6dcbd1
aandreluis <andreluisstn@gmail.com>
2025-01-15 22:18:13 -0300

    feat: page design improvements
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/LoadingSkeleton.jsx
+++ Frontend/4inFrontend/src/components/LoadingSkeleton.jsx
@@ -1,3 +1,5 @@
+import { Rat } from "lucide-react";
+
 export function LoadingSkeleton() {
   const Item = ({ width, bgColor }) => (
     <div className="flex items-center py-2">
@@ -6,30 +8,16 @@ export function LoadingSkeleton() {
   );
 
   return (
-    <div className="flex animate-pulse flex-col gap-8">
-      <div className="flex justify-evenly bg-neutral-100">
-        <Item width="w-16" bgColor="bg-neutral-200" />
-        <Item width="w-32" bgColor="bg-neutral-300" />
-        <Item width="w-36" bgColor="bg-neutral-300" />
-        <Item width="w-24" bgColor="bg-neutral-100" />
-        <Item width="w-32" bgColor="bg-neutral-200" />
-        <Item width="w-18" bgColor="bg-neutral-300" />
-      </div>
-
-      <div className="flex justify-evenly bg-neutral-200">
-        <Item width="w-60" bgColor="bg-neutral-100" />
-        <Item width="w-36" bgColor="bg-neutral-300" />
-        <Item width="w-40" bgColor="bg-neutral-100" />
-        <Item width="w-20" bgColor="bg-neutral-300" />
-      </div>
-
-      <div className="flex justify-evenly bg-neutral-100">
+    <div className="flex animate-pulse flex-col items-center gap-8">
+      <div className="flex w-full justify-evenly bg-neutral-100">
         <Item width="w-36" bgColor="bg-neutral-200" />
-        <Item width="w-40" bgColor="bg-neutral-300" />
-        <Item width="w-50" bgColor="bg-neutral-300" />
-        <Item width="w-48" bgColor="bg-neutral-200" />
+        <Item width="w-80" bgColor="bg-neutral-300" />
+        <Item width="w-40" bgColor="bg-neutral-200" />
+
         <Item width="w-18" bgColor="bg-neutral-300" />
       </div>
+
+      <Rat size={48} className="animate-bounce text-neutral-700" />
     </div>
   );
 }
