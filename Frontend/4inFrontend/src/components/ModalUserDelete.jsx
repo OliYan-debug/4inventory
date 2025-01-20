87b6116b2ed327bafdc296a710d2ff206ca6f29b
aandreluis <andreluisstn@gmail.com>
2025-01-19 22:53:15 -0300

    feat: users can now be deleted.
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/ModalUserDelete.jsx
+++ Frontend/4inFrontend/src/components/ModalUserDelete.jsx
@@ -1,6 +1,56 @@
 import { Trash2 } from "lucide-react";
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
 
-export function ModalUserDelete({ userId, userName, setCheckDeleteOpen }) {
+export function ModalUserDelete({
+  username,
+  name,
+  setCheckDeleteOpen,
+  updateData,
+}) {
+  const { user } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleDeleteUser = async () => {
+    const data = {
+      username,
+    };
+
+    try {
+      const result = await toast.promise(api.delete("/admin/users", { data }), {
+        pending: "Deleting user",
+        success: {
+          render() {
+            return <p>User deleted!</p>;
+          },
+        },
+        error: {
+          render({ data }) {
+            return (
+              <p>
+                Error when deleting:
+                <span className="font-bold">{data.response.data.message}</span>.
+                Try again.
+              </p>
+            );
+          },
+        },
+      });
+
+      if (result.status === 200 && user.sub === username) {
+        return navigate("/logout");
+      }
+
+      updateData();
+      setCheckDeleteOpen(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="fixed inset-0 z-50 flex h-screen w-screen animate-fadeIn items-center justify-center bg-black/50">
       <div className="flex w-4/5 flex-col items-center rounded-2xl bg-neutral-50 px-8 py-6 text-center md:w-[40vw]">
@@ -10,10 +60,18 @@ export function ModalUserDelete({ userId, userName, setCheckDeleteOpen }) {
         <h1 className="my-2 text-2xl font-bold text-neutral-800">
           Delete User?
         </h1>
-        <span className="font-medium text-red-600">{userName}</span>
+        <span className="font-medium text-red-600">{name}</span>
         <p className="text-sm text-neutral-500">
           Will be deleted, this action cannot be undone.
         </p>
+
+        {user.sub === username && (
+          <div className="mt-4 text-xs text-neutral-400">
+            You are deleting your own account,{" "}
+            <span className="font-semibold">you will be disconnected.</span>
+          </div>
+        )}
+
         <div className="mt-4 flex gap-2">
           <button
             type="button"
@@ -25,7 +83,7 @@ export function ModalUserDelete({ userId, userName, setCheckDeleteOpen }) {
 
           <button
             type="button"
-            onClick={() => console.log(userId)}
+            onClick={() => handleDeleteUser()}
             className="flex w-32 items-center justify-center rounded-lg bg-red-400 px-2 py-1 font-semibold text-neutral-50 transition hover:bg-red-500 hover:underline"
           >
             Yes, delete
