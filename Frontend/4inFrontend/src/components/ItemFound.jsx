08fdc26640d7bcda1fbe3ee74f67fe89a1c93e42
aandreluis <andreluisstn@gmail.com>
2025-02-10 23:09:48 -0300

    feat: adds focus design
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/ItemFound.jsx
+++ Frontend/4inFrontend/src/components/ItemFound.jsx
@@ -8,11 +8,14 @@ export function ItemFound({ id, item, category, setOpenSearch }) {
       onClick={() => {
         setOpenSearch(false);
       }}
+      className="group rounded-lg focus-visible:outline-offset-4 focus-visible:outline-sky-500"
     >
-      <li className="group flex h-16 w-full animate-fadeIn cursor-pointer flex-row items-center gap-3 rounded-lg bg-neutral-300 px-2 transition hover:bg-sky-400">
-        <span className="flex size-8 items-center justify-center rounded-lg border border-neutral-500 group-hover:border-neutral-50">
+      <li className="flex h-16 w-full animate-fadeIn cursor-pointer flex-row items-center gap-3 rounded-lg bg-neutral-300 px-2 transition hover:bg-sky-400 group-focus-visible:bg-sky-400">
+        <span className="flex size-8 items-center justify-center rounded-lg border border-neutral-500 group-hover:border-neutral-50 group-focus-visible:border-neutral-50">
           <Box
-            className={"text-neutral-700 group-hover:text-neutral-50"}
+            className={
+              "text-neutral-700 group-hover:text-neutral-50 group-focus-visible:text-neutral-50"
+            }
             size={16}
           />
         </span>
@@ -23,22 +26,24 @@ export function ItemFound({ id, item, category, setOpenSearch }) {
               return (
                 <div
                   key={category.id}
-                  className="rounded-md bg-neutral-400 px-1 py-px group-hover:bg-neutral-50"
+                  className="rounded-md bg-neutral-400 px-1 py-px group-hover:bg-neutral-50 group-focus-visible:bg-neutral-50"
                 >
-                  <p className="text-xs text-neutral-50 drop-shadow-sm group-hover:text-neutral-500">
+                  <p className="text-xs text-neutral-50 drop-shadow-sm group-hover:text-neutral-500 group-focus-visible:text-neutral-500">
                     {category.name}
                   </p>
                 </div>
               );
             })}
           </div>
-          <p className="font-medium text-neutral-700 group-hover:text-neutral-50">
+          <p className="font-medium text-neutral-700 group-hover:text-neutral-50 group-focus-visible:text-neutral-50">
             {item}
           </p>
         </div>
 
         <ChevronRight
-          className={"ml-auto text-neutral-700 group-hover:text-neutral-50"}
+          className={
+            "ml-auto text-neutral-700 group-hover:text-neutral-50 group-focus-visible:text-neutral-50"
+          }
           size={22}
         />
       </li>
