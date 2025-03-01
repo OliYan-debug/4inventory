4d7f2b00e9109a8fe32950ef882173a7193eb00c
aandreluis <andreluisstn@gmail.com>
2025-02-28 22:07:04 -0300

    feat: adds translation to the Update item page and components.
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/routes/UpdateItem.jsx
+++ Frontend/4inFrontend/src/routes/UpdateItem.jsx
@@ -7,10 +7,13 @@ import { api } from "../services/api";
 import { Header } from "../components/Header";
 import { ModalCategoriesError } from "../components/ModalCategoriesError";
 import { InputCategories } from "../components/InputCategories";
-import { InputUpdateItemName } from "../components/InputUpdateItemName";
+import { InputSearchItems } from "../components/InputSearchItems";
 import { InputDescription } from "../components/InputDescription";
+import { useTranslation } from "react-i18next";
 
 export default function UpdateItem() {
+  const { t } = useTranslation("update_item");
+
   let { itemId } = useParams();
   const [categories, setCategories] = useState([]);
   const [getCategoriesError, setGetCategoriesError] = useState(false);
@@ -47,7 +50,7 @@ export default function UpdateItem() {
           toast.warning(
             <p>
               <span className="font-bold">{error.response.data.message}</span>{" "}
-              search other item{" "}
+              {t("fetch_error")}
             </p>,
             {
               toastId: itemId,
@@ -66,7 +69,7 @@ export default function UpdateItem() {
         setCategories(response.data);
       })
       .catch((error) => {
-        toast.error("Error getting categories, try again");
+        toast.error(t("categoriesError.toastError"));
         setGetCategoriesError(error);
         console.error(error);
       });
@@ -76,7 +79,7 @@ export default function UpdateItem() {
     if (selectedCategories.length === 0) {
       setError(
         "category",
-        { type: "required", message: "Category is required" },
+        { type: "required", message: t("form.category_required") },
         { shouldFocus: true },
       );
       return;
@@ -91,12 +94,13 @@ export default function UpdateItem() {
 
     try {
       await toast.promise(api.put("/inventory", data), {
-        pending: "Updating item",
+        pending: t("loading.pending"),
         success: {
           render() {
             return (
               <p>
-                Item <span className="font-bold">{data.item}</span> updated!
+                {t("loading.success")}{" "}
+                <span className="font-bold">{data.item}</span>
               </p>
             );
           },
@@ -105,9 +109,11 @@ export default function UpdateItem() {
           render({ data }) {
             return (
               <p>
-                Error when updating:
-                <span className="font-bold">{data.response.data.message}</span>.
-                Try again.
+                {t("loading.error")}
+                <br />
+                <span className="text-xs opacity-80">
+                  {data.response.data.message}
+                </span>
               </p>
             );
           },
@@ -119,32 +125,34 @@ export default function UpdateItem() {
     }
   };
 
-  const subtitle = () => {
+  const Subtitle = () => {
     return (
       <p className="flex items-center text-sm text-neutral-500">
         <Link to={`/products`} className="hover:font-semibold">
-          Products
+          {t("breadcrumb.products")}
         </Link>
         <ChevronRight size={16} color="#737373" />
-        <span className="font-semibold">Update Product</span>
+        <span className="font-semibold">{t("breadcrumb.updateItem")}</span>
       </p>
     );
   };
 
   return (
     <div className="flex flex-col gap-4">
-      <Header title={"Update Item"} subtitle={subtitle()} />
+      <Header title={t("title")} subtitle={Subtitle()} />
 
       {getCategoriesError !== false ? (
         <div className="flex min-h-screen max-w-full flex-col items-center rounded-2xl bg-neutral-50 px-8 py-4 text-center">
-          <h1 className="text-3xl font-bold text-neutral-800">Oops!</h1>
+          <h1 className="text-3xl font-bold text-neutral-800">
+            {t("categoriesError.title")}
+          </h1>
           <p className="mb-4 mt-2 text-sm text-neutral-500">
-            Sorry, an unexpected error has occurred:
+            {t("categoriesError.text")}
           </p>
           <span className="mb-6 font-medium text-neutral-600">
             {getCategoriesError.statusText || getCategoriesError.message}
           </span>
-          <ServerOff size={84} color="#262626" />
+          <ServerOff size={84} className="text-neutral-800" />
         </div>
       ) : (
         <div className="flex min-h-screen max-w-full justify-center rounded-2xl bg-neutral-50 p-4">
@@ -154,7 +162,7 @@ export default function UpdateItem() {
             onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col items-center gap-2 pt-8 md:w-1/2"
           >
-            <InputUpdateItemName
+            <InputSearchItems
               register={register}
               errors={errors}
               clearErrors={clearErrors}
@@ -189,14 +197,14 @@ export default function UpdateItem() {
               className="mt-10 flex items-center justify-center rounded-lg bg-emerald-400 px-4 py-2 font-semibold text-neutral-50 transition hover:bg-emerald-500 disabled:cursor-no-drop disabled:opacity-70 md:w-2/5"
             >
               <CirclePlus size={20} className="me-2 text-neutral-50" />
-              Update item
+              {t("buttons.submit")}
             </button>
 
             <Link
               to={"/products"}
               className="flex items-center font-semibold text-neutral-400 hover:underline hover:opacity-80"
             >
-              Back to products
+              {t("buttons.back")}
               <Undo2 size={20} className="ms-1 text-neutral-400" />
             </Link>
           </form>
