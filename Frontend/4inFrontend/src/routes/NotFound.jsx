0a76b25df934a084bde776f7d4a01a3dd6ea87ad
aandreluis <andreluisstn@gmail.com>
2025-04-23 19:38:35 -0300

    feat: adds translation to the NotFound page
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/routes/NotFound.jsx
+++ Frontend/4inFrontend/src/routes/NotFound.jsx
@@ -1,20 +1,23 @@
 import { SearchX } from "lucide-react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router";
 
 export default function NotFound() {
+  const { t } = useTranslation("not_found");
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-neutral-50">
       <h1 className="text-8xl font-bold text-neutral-800">404</h1>
-      <h3 className="text-2xl">Somethings missing.</h3>
-      <p className="mb-4 mt-2 text-neutral-500">
-        Sorry, we cant find that page.
-      </p>
-      <SearchX size={84} color="#262626" />
+      <h3 className="text-2xl font-semibold">{t("title")}</h3>
+      <p className="mb-4 mt-2 text-neutral-500">{t("subtitle")}</p>
+
+      <SearchX className="size-24 text-neutral-800" />
+
       <Link
         to={"/"}
         className="my-2 rounded-lg bg-neutral-600 px-3 py-2 font-medium text-neutral-50 transition hover:opacity-80"
       >
-        Back to Homepage
+        {t("button.back")}
       </Link>
     </div>
   );
