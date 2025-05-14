52a05bd94a8239acc63fb067546db2da5d88ae45
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2025-05-14 12:18:42 -0300

    feat: Add backup service to handle export data to json
    Signed-off-by: OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>

--- Backend/4inventory-api/src/main/java/_inventory/_inventory_api/services/InventoryService.java
+++ Backend/4inventory-api/src/main/java/_inventory/_inventory_api/services/InventoryService.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -39,6 +40,10 @@ public class InventoryService {
         Optional<InventoryItem> optionalItem = inventoryRepo.findById(itemId);
         if (optionalItem.isPresent()) return optionalItem.get();
         throw new ItemIdNotFoundException(itemId);
+    }
+
+    public List<InventoryItem> findAll(){
+        return inventoryRepo.findAll();
     }
 
     public InventoryItem saveItem(InventoryItem item) {
