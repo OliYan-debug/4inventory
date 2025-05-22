b0392f8e02af5583cb8b25d74127f6d22147bee7
aandreluis <andreluisstn@gmail.com>
2025-05-21 22:22:34 -0300

    feat: adds more information (quantity and description), accessibility (select the item using the keyboard) and design changes to the search list items
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/ItemSearch.jsx
+++ Frontend/4inFrontend/src/components/ItemSearch.jsx
@@ -1,12 +1,24 @@
+import { PackageCheck, PackageX } from "lucide-react";
+
 export function ItemSearch({ id, item, handleSelect }) {
   return (
-    <li
-      onClick={() => handleSelect(id)}
-      className="cursor-pointer px-4 transition hover:bg-neutral-200"
-    >
-      <div className="py-2">
-        <p className="text-neutral-500">{item}</p>
-      </div>
+    <li>
+      <button
+        type="button"
+        onClick={() => handleSelect(id)}
+        className="w-full cursor-pointer border-b border-neutral-200 px-4 outline-none transition focus-within:bg-neutral-300 hover:bg-neutral-100"
+      >
+        <div className="flex items-center gap-1 py-2 text-neutral-500">
+          {item.quantity <= 0 ? (
+            <PackageCheck className="size-5 min-w-5" />
+          ) : (
+            <PackageX className="size-5 min-w-5" />
+          )}
+
+          <p className="min-w-20 truncate font-bold">{item.item}</p>
+          <p className="truncate text-xs italic">{item.description}</p>
+        </div>
+      </button>
     </li>
   );
 }
