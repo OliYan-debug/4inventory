02e9e476bb405de55ca828502afe0dd0e5177f2f
aandreluis <andreluisstn@gmail.com>
2025-05-23 19:58:42 -0300

    feat/refact: adds an identification for items no stock; if the item does not have quantity the checkout button will be disabled; changes the design of the categories with a focus on the contrast of the text color and refactors the 'Item Menu' into a separate component
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/components/Item.jsx
+++ Frontend/4inFrontend/src/components/Item.jsx
@@ -1,16 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
+  Calendar,
   Ellipsis,
-  PackageMinus,
-  PackageOpen,
-  PackagePlus,
   PackageX,
-  Pencil,
   SquareArrowOutUpRight,
 } from "lucide-react";
-import useAuth from "../hooks/useAuth";
 import { useTranslation } from "react-i18next";
+import { ItemMenu } from "./ItemMenu";
+import { getContrastingTextColor } from "../utils/getContrast";
 
 export function Item({
   id,
@@ -21,86 +19,20 @@ export function Item({
   createdAt,
   count,
 }) {
-  const { t } = useTranslation("item");
+  const { i18n } = useTranslation("item");
+  const currentLanguage = i18n.language;
 
   const [itemMenuOpen, setItemMenuOpen] = useState(false);
-  const ref = useRef(null);
-
-  const { user } = useAuth();
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  useEffect(() => {
-    if (user) {
-      setIsAdmin(user.role === "ADMIN");
-    }
-  }, [user]);
-
-  const handleClickOutside = (event) => {
-    if (ref.current && !ref.current.contains(event.target)) {
-      setItemMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside, true);
-    return () => {
-      document.removeEventListener("click", handleClickOutside, true);
-    };
-  }, []);
 
   function handleOpenItemMenu() {
     setItemMenuOpen(true);
   }
 
-  const ItemMenu = () => {
-    return (
-      <div
-        ref={ref}
-        className="absolute left-1 top-5 z-10 animate-fadeIn rounded-lg bg-neutral-50 py-2 shadow-lg"
-      >
-        <ul className="flex w-36 flex-col gap-1 text-neutral-600">
-          <li className="flex h-7 w-full cursor-pointer items-center px-4 transition hover:bg-neutral-400 hover:font-medium">
-            <Link to={`item/${id}`} className="flex w-full items-center">
-              <PackageOpen size={19} className="me-1" />
-              {t("menu.see")}
-            </Link>
-          </li>
-
-          <li className="flex h-7 w-full cursor-pointer items-center px-4 transition hover:bg-neutral-400 hover:font-medium">
-            <Link to={`update/${id}`} className="flex w-full items-center">
-              <Pencil size={19} className="me-1" />
-              {t("menu.edit")}
-            </Link>
-          </li>
-
-          <li className="flex h-7 w-full cursor-pointer items-center px-4 transition hover:bg-neutral-400 hover:font-medium">
-            <Link to={`checkin/${id}`} className="flex w-full items-center">
-              <PackagePlus size={19} className="me-1" />
-              {t("menu.checkin")}
-            </Link>
-          </li>
-          <li className="flex h-7 w-full cursor-pointer items-center px-4 transition hover:bg-neutral-400 hover:font-medium">
-            <Link to={`checkout/${id}`} className="flex w-full items-center">
-              <PackageMinus size={19} className="me-1" />
-              {t("menu.checkout")}
-            </Link>
-          </li>
-          {isAdmin && (
-            <li className="flex h-7 w-full cursor-pointer items-center border-t px-4 py-2 text-red-500 transition hover:bg-red-300 hover:font-medium">
-              <Link to={`delete/${id}`} className="flex w-full items-center">
-                <PackageX size={19} className="me-1" />
-                {t("menu.delete")}
-              </Link>
-            </li>
-          )}
-        </ul>
-      </div>
-    );
-  };
+  const formatDate = (value) => value.toString().padStart(2, "0");
 
   return (
     <div
-      className={`relative grid min-w-[840px] animate-fadeIn grid-cols-7 items-center justify-items-center text-wrap ${
+      className={`relative grid min-h-12 min-w-[840px] animate-fadeIn grid-cols-7 items-center justify-items-center text-wrap text-neutral-500 ${
         count % 2 ? "bg-neutral-100" : "bg-neutral-200"
       }`}
     >
@@ -111,19 +43,25 @@ export function Item({
           disabled={itemMenuOpen}
           className="transition hover:opacity-60 disabled:cursor-no-drop disabled:opacity-60"
         >
-          <Ellipsis color="#737373" size={20} />
+          <Ellipsis className="size-5 text-neutral-500" />
         </button>
 
-        {itemMenuOpen && <ItemMenu />}
+        {itemMenuOpen && (
+          <ItemMenu
+            id={id}
+            quantity={quantity}
+            setItemMenuOpen={setItemMenuOpen}
+          />
+        )}
       </div>
 
       <div className="col-auto flex items-center py-2">
-        <p className="text-neutral-500">{id}</p>
+        <p>{id}</p>
       </div>
 
       <div className="col-auto flex items-center py-2">
         <Link to={`item/${id}`}>
-          <p className="group flex items-center gap-1 text-neutral-500 underline transition hover:font-semibold">
+          <p className="group flex items-center gap-1 underline transition hover:font-semibold">
             {item}
             <SquareArrowOutUpRight
               size={12}
@@ -134,39 +72,65 @@ export function Item({
       </div>
 
       <div className="group/description relative col-span-2 flex items-center justify-center py-2">
-        <p className="text-neutral-500">
+        <p>
           {description.length > 20
             ? `${description.substring(0, 24)}...`
             : description}
         </p>
 
-        <div className="absolute top-10 z-10 hidden max-w-72 animate-fadeIn justify-center overflow-x-clip rounded-lg border border-neutral-500 bg-neutral-400 p-2 shadow-md group-hover/description:flex">
-          <span className="absolute -top-1 block size-2 -translate-y-px rotate-45 border-l border-t border-neutral-500 bg-neutral-400"></span>
-          <p className="text-justify text-xs text-neutral-50">{description}</p>
-        </div>
+        {description.length > 20 && (
+          <div className="absolute top-10 z-10 hidden max-w-72 animate-fadeIn justify-center overflow-x-clip rounded-lg border border-neutral-500 bg-neutral-400 p-2 shadow-md group-hover/description:flex">
+            <span className="absolute -top-1 block size-2 -translate-y-px rotate-45 border-l border-t border-neutral-500 bg-neutral-400"></span>
+            <p className="text-justify text-xs text-neutral-50">
+              {description}
+            </p>
+          </div>
+        )}
       </div>
 
-      <div className="col-auto flex flex-col items-center gap-1 py-2">
+      <div className="col-auto flex flex-col items-center gap-0.5 py-2">
         {categories.map((category) => {
           return (
             <div
               key={category.id}
               style={{ backgroundColor: category.color }}
-              className="rounded-md px-1 py-px text-sm"
+              className="w-32 max-w-32 truncate rounded-2xl px-2 py-0.5 text-center text-xs"
             >
-              <p className="text-neutral-300 drop-shadow-sm">{category.name}</p>
+              <p
+                className={`getContrastingTextColor truncate drop-shadow-sm ${getContrastingTextColor(category.color)}`}
+              >
+                {category.name}
+              </p>
             </div>
           );
         })}
       </div>
 
       <div className="col-auto flex items-center py-2">
-        <p className="text-neutral-500">{quantity}</p>
+        <p
+          className={`flex items-center gap-0.5 ${quantity <= 0 ? "font-semibold text-red-700" : "text-neutral-500"}`}
+        >
+          {quantity}
+          {quantity <= 0 && <PackageX className="size-5" />}
+        </p>
       </div>
 
       <div className="col-auto flex items-center py-2">
-        <p className="text-neutral-500">
-          {createdAt[0]}-{createdAt[1]}-{createdAt[2]}
+        <p className="flex items-center gap-0.5">
+          <>
+            {currentLanguage === "en-US" ? (
+              <>
+                {createdAt[0]}/{formatDate(createdAt[1])}/
+                {formatDate(createdAt[2])}
+              </>
+            ) : (
+              <>
+                {formatDate(createdAt[2])}/{formatDate(createdAt[1])}/
+                {createdAt[0]}
+              </>
+            )}
+          </>
+          <Calendar className="size-4" />
         </p>
       </div>
     </div>
