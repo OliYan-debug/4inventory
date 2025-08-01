72f97025fc22b6de960f89b59bbaf9209cd454bf
aandreluis <andreluisstn@gmail.com>
2025-08-01 17:32:09 -0300

    feat: adds img background
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/routes/Root.jsx
+++ Frontend/4inFrontend/src/routes/Root.jsx
@@ -4,7 +4,7 @@ import { MobileHeader } from "../components/MobileHeader";
 
 function root() {
   return (
-    <div className="m-0 flex max-w-full flex-row bg-neutral-300 p-4">
+    <div className="m-0 flex min-h-dvh max-w-full flex-row bg-neutral-300 bg-[url(./src/assets/wave_background.svg)] bg-cover bg-no-repeat p-4">
       <NavBar />
 
       <div id="content" className="h-full w-full">
