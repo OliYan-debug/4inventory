90d92d06f7a286d87e53c2c8676a7430167c226d
aandreluis <andreluisstn@gmail.com>
2025-11-05 20:32:11 -0300

    feat: adds file type validation and upload function
    Signed-off-by: aandreluis <andreluisstn@gmail.com>

--- Frontend/4inFrontend/src/routes/backup/components/UploadBackup.jsx
+++ Frontend/4inFrontend/src/routes/backup/components/UploadBackup.jsx
@@ -1,10 +1,13 @@
-import { FileIcon, UploadIcon, X } from "lucide-react";
+import { FileIcon, FileX, UploadIcon, X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import { Button } from "@/components/Button";
+import { api } from "@/services/api";
+import { toast } from "react-toastify";
 
 export function UploadBackup() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [invalidFile, setInvalidFile] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
 
   function onFileSelected(event) {
@@ -12,12 +15,10 @@ export function UploadBackup() {
 
     const { files } = event.target;
 
-    if (!files) {
-      return;
-    }
-
-    if (files && files[0]) {
+    if (files && files[0] && files[0].type.startsWith("application/json")) {
       setSelectedFile(files[0]);
+    } else {
+      setInvalidFile(true);
     }
   }
 
@@ -28,8 +29,15 @@ export function UploadBackup() {
 
     const droppedFiles = e.dataTransfer.files;
 
-    if (droppedFiles && droppedFiles[0]) {
+    if (
+      droppedFiles &&
+      droppedFiles[0] &&
+      droppedFiles[0].type.startsWith("application/json")
+    ) {
       setSelectedFile(droppedFiles[0]);
+    } else {
+      setIsDragging(false);
+      setInvalidFile(true);
     }
   };
 
@@ -42,9 +50,67 @@ export function UploadBackup() {
     setIsDragging(false);
   };
 
+  // const [processedData, setProcessedData] = useState(null);
+
+  async function processFileData() {
+    if (!selectedFile) return;
+
+    try {
+      const text = await selectedFile.text();
+      const data = JSON.parse(text);
+      return data;
+    } catch (error) {
+      console.error("Erro ao ler o arquivo JSON:", error);
+      setInvalidFile();
+    }
+  }
+
+  async function onSubmit() {
+    const data = await processFileData();
+
+    try {
+      await toast.promise(api.post("/backup", data), {
+        pending: "loading.pending",
+        success: {
+          render() {
+            return <p>{"loading.success"}</p>;
+          },
+        },
+        error: {
+          render({ data }) {
+            return (
+              <p>
+                {"loading.error"}
+                <br />
+                <span className="text-xs opacity-80">{data.message}</span>
+              </p>
+            );
+          },
+        },
+      });
+    } catch (error) {
+      console.error(error);
+
+      setInvalidFile(true);
+      setSelectedFile(null);
+    }
+  }
+
   useEffect(() => {
     selectedFile && setIsDragging(true);
   }, [selectedFile]);
+
+  useEffect(() => {
+    if (!invalidFile) return;
+
+    let timeout = window.setTimeout(() => {
+      setInvalidFile(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [invalidFile]);
 
   return (
     <div className="animate-fade-in flex w-full flex-col items-center gap-4">
@@ -67,10 +133,13 @@ export function UploadBackup() {
           onDragEnter={() => setIsDragging(true)}
           onDragLeave={() => setIsDragging(false)}
           data-dragging={isDragging}
-          className="group flex h-64 w-full flex-col items-center justify-center gap-1 rounded-lg border-2 border-dashed bg-neutral-100 transition hover:bg-neutral-200/40 data-[dragging=true]:border-sky-500 data-[dragging=true]:bg-neutral-200"
+          data-invalid={invalidFile}
+          className="group flex h-64 w-full flex-col items-center justify-center gap-1 rounded-lg border-2 border-dashed bg-neutral-100 transition hover:bg-neutral-200/40 data-[dragging=true]:border-sky-500 data-[dragging=true]:bg-neutral-200 data-[invalid=true]:border-red-400"
         >
-          <span className="flex size-14 items-center justify-center rounded-full bg-neutral-200 text-neutral-500 group-data-[dragging=true]:bg-neutral-100 group-data-[dragging=true]:text-sky-400">
-            {selectedFile ? (
+          <span className="flex size-14 items-center justify-center rounded-full bg-neutral-200 text-neutral-500 group-data-[dragging=true]:bg-neutral-100 group-data-[dragging=true]:text-sky-400 group-data-[invalid=true]:text-red-400">
+            {invalidFile ? (
+              <FileX className="size-4" />
+            ) : selectedFile ? (
               <FileIcon className="size-4" />
             ) : (
               <UploadIcon className="size-4" />
@@ -81,9 +150,13 @@ export function UploadBackup() {
             <span className="max-w-64 truncate text-center text-sm text-neutral-500 italic">
               {selectedFile.name}
             </span>
-          ) : (
+          ) : !invalidFile ? (
             <span className="text-center text-neutral-500">
               Drop the backup file here, or click to browse
+            </span>
+          ) : (
+            <span className="text-center text-red-400">
+              Invalid backup file. Only accept json file
             </span>
           )}
 
@@ -92,7 +165,7 @@ export function UploadBackup() {
             type="file"
             id="file-input"
             multiple
-            accept="image/*"
+            accept=".json"
             onChange={(event) => {
               onFileSelected(event);
 
@@ -102,7 +175,10 @@ export function UploadBackup() {
         </label>
       </div>
 
-      <Button className="w-64 bg-sky-400">
+      <Button
+        className="w-64 bg-sky-400"
+        onClick={async () => await onSubmit()}
+      >
         Upload <UploadIcon className="size-4" />
       </Button>
     </div>
